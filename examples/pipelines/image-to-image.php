c184b25dbfb5c6cdb691d6efe8a3c48cb6becbc7
Kyrian Obikwelu <koshnawaza@gmail.com>
2024-04-10 11:08:46 +0100

    Change image to image pipeline to accept a save path for the output

--- examples/pipelines/image-to-image.php
+++ examples/pipelines/image-to-image.php
@@ -2,7 +2,6 @@
 
 declare(strict_types=1);
 
-use Codewithkyrian\Transformers\Generation\Streamers\StdOutStreamer;
 use function Codewithkyrian\Transformers\Pipelines\pipeline;
 use function Codewithkyrian\Transformers\Utils\memoryUsage;
 use function Codewithkyrian\Transformers\Utils\timeUsage;
@@ -13,10 +12,10 @@ ini_set('memory_limit', '2048M');
 
 $upscaler = pipeline('image-to-image', 'Xenova/swin2SR-classical-sr-x2-64');
 
-$url = __DIR__. '/../images/butterfly.jpg';
+$url = __DIR__ . '/../images/butterfly.jpg';
 
-$output = $upscaler($url);
+$savePath = __DIR__ . '/../images/butterfly-super-resolution.jpg';
 
-$output->save(__DIR__. '/../images/butterfly-super-resolution.jpg');
+$output = $upscaler($url, saveTo: $savePath);
 
-dd($output->size(), timeUsage(), memoryUsage());
+dd($output, timeUsage(), memoryUsage());
--- src/Pipelines/ImageToImagePipeline.php
+++ src/Pipelines/ImageToImagePipeline.php
@@ -7,9 +7,9 @@ namespace Codewithkyrian\Transformers\Pipelines;
 
 use Codewithkyrian\Transformers\Utils\Image;
 use Codewithkyrian\Transformers\Utils\Tensor;
+use Exception;
 use Interop\Polite\Math\Matrix\NDArray;
 use function Codewithkyrian\Transformers\Utils\prepareImages;
-use function Codewithkyrian\Transformers\Utils\timeUsage;
 
 /**
  * Image to Image pipeline using any `AutoModelForImageToImage`. This pipeline generates an image based on a previous image input.
@@ -18,13 +18,13 @@ use function Codewithkyrian\Transformers\Utils\timeUsage;
  * ```php
  * $upscaler = pipeline('image-to-image', 'Xenova/swin2SR-classical-sr-x2-64');
  * $url = 'https://huggingface.co/datasets/Xenova/transformers.js-docs/resolve/main/butterfly.jpg';
- * $output = $upscaler($url);
- * // Image {
- * //   data: array(786432) [ 41, 31, 24,  43, ... ],
+ * $output = $upscaler($url, saveTo: 'butterfly-super-resolution.jpg');
+ * // [
+ * //   path: 'butterfly-super-resolution.jpg',
  * //   width: 512,
  * //   height: 512,
  * //   channels: 3
- * // }
+ * // ]
  * ```
  */
 class ImageToImagePipeline extends Pipeline
@@ -34,10 +34,20 @@ class ImageToImagePipeline extends Pipeline
      * @param array|string $inputs
      * @param mixed ...$args
      * @return Image|Image[]
-     * @throws \Exception
+     * @throws Exception
      */
     public function __invoke(array|string $inputs, ...$args): array|Image
     {
+        $saveTo = $args[0] ?? $args['saveTo'] ?? null;
+
+        if (!$saveTo) {
+            throw new Exception('You must provide a save path');
+        }
+
+        if (!is_array($saveTo)) {
+            $saveTo = [$saveTo];
+        }
+
         $preparedImages = prepareImages($inputs);
 
         $inputs = ($this->processor)($preparedImages);
@@ -47,14 +57,23 @@ class ImageToImagePipeline extends Pipeline
         $toReturn = [];
 
         /** @var Tensor $batch */
-        foreach ($outputs['reconstruction'] as $batch) {
+        foreach ($outputs['reconstruction'] as $i => $batch) {
             $output = $batch->squeeze()
                 ->clamp(0, 1)
                 ->multiplyScalar(255)
                 ->round()
                 ->to(NDArray::uint8);
 
-            $toReturn[] = Image::fromTensor($output);
+            $image = Image::fromTensor($output);
+
+            $image->save($saveTo[$i]);
+
+            $toReturn[] = [
+                'path' => $saveTo[$i],
+                'width' => $image->width(),
+                'height' => $image->height(),
+                'channels' => $image->channels,
+            ];
         }
 
         return count($toReturn) > 1 ? $toReturn : $toReturn[0];
