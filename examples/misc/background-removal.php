563c0f56aa54a61b193ac871572119762dc482bd
Kyrian Obikwelu <koshnawaza@gmail.com>
2024-08-29 10:09:24 +0100

    feat: Update background removal example

--- examples/misc/background-removal.php
+++ examples/misc/background-removal.php
@@ -8,24 +8,11 @@ use Codewithkyrian\Transformers\Utils\Image;
 use function Codewithkyrian\Transformers\Utils\{memoryPeak, memoryUsage, timeUsage};
 
 require_once './bootstrap.php';
-$modelConfig = ['model_type' => 'vit'];
-$processorConfig = [
-    'do_normalize' => true,
-    'do_pad' => false,
-    'do_rescale' => true,
-    'do_resize' => true,
-    'image_mean' => [0.5, 0.5, 0.5],
-    'feature_extractor_type' => "ImageFeatureExtractor",
-    'image_std' => [1, 1, 1],
-    'resample' => 2,
-    'rescale_factor' => 0.00392156862745098,
-    'size' => ['width' => 1024, 'height' => 1024],
-];
 
-$model = AutoModel::fromPretrained(modelNameOrPath: 'briaai/RMBG-1.4', config: $modelConfig);
+$model = AutoModel::fromPretrained(modelNameOrPath: 'briaai/RMBG-1.4');
 $processor = AutoProcessor::fromPretrained(modelNameOrPath: 'briaai/RMBG-1.4');
 
-$url = __DIR__ . '/../images/multitask.png';
+$url = __DIR__ . '/../images/woman-w-bag.jpeg';
 
 $image = Image::read($url);
 
@@ -34,11 +21,11 @@ $fileName = pathinfo($url, PATHINFO_FILENAME);
 ['pixel_values' => $pixelValues] = $processor($image);
 
 ['output' => $output] = $model(['input' => $pixelValues]);
-
+//
 $mask = Image::fromTensor($output[0]->multiply(255))->resize($image->width(), $image->height());
-
+//
 $mask->save($fileName . '-mask.png');
-
+//
 $maskedImage = $image->applyMask($mask);
-
-$maskedImage->save($fileName . '-masked.png');
+//
+//$maskedImage->save($fileName . '-masked.png');
